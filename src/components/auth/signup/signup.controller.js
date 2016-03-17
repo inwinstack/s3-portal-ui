179ad2122d87ffd966fdd6c727158cfe018326f1
jigsawye <evan.y@inwinstack.com>
2016-03-17 11:42:51 +0800

    Add the new UI to SignUp module
    Integrate the new UI design
    
    * Update SignUpController for new UI.
    * Update ng attributes of signup.html.

--- src/components/auth/signup/signup.controller.js
+++ src/components/auth/signup/signup.controller.js
@@ -1,9 +1,39 @@
 export default class SignUpController {
   /** @ngInject */
-  constructor($auth, $state, $toast) {
+  constructor($auth, $state, $toast, $fetch) {
     Object.assign(this, {
-      $auth, $state, $toast,
+      $auth, $state, $toast, $fetch, credentials: {},
     });
+  }
+
+  /**
+   * Check whether the email is used.
+   * @return void
+   */
+  checkEmail() {
+    if (this.form.email.$valid) {
+      this.checkEmailRequest();
+    }
+  }
+
+  /**
+   * Send a request to server for check the email.
+   * @return void
+   */
+  checkEmailRequest() {
+    const { email } = this.credentials;
+    this.isCheckEmail = true;
+
+    this.$fetch.post('/v1/auth/checkEmail', { email })
+      .then(() => {
+        this.emailIsValid = true;
+        this.emailIsInvalid = false;
+      })
+      .catch(() => {
+        this.emailIsValid = false;
+        this.emailIsInvalid = true;
+      })
+      .finally(() => (this.isCheckEmail = false));
   }
 
   /**
