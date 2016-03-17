e7ebee8b11a6fcab702051fa9068fae01eee8727
jigsawye <evan.y@inwinstack.com>
2016-03-17 11:42:24 +0800

    Fix sign in & sign up uri

--- src/config/satellizer.config.js
+++ src/config/satellizer.config.js
@@ -2,6 +2,6 @@
 export default ($authProvider, Config) => {
   const { API_URL } = Config;
 
-  $authProvider.loginUrl = `${API_URL}/api/v1/auth/login`;
-  $authProvider.signupUrl = `${API_URL}/api/v1/auth/register`;
+  $authProvider.loginUrl = `${API_URL}/v1/auth/login`;
+  $authProvider.signupUrl = `${API_URL}/v1/auth/register`;
 };
