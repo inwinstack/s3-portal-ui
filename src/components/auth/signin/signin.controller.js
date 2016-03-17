77f74aa000a234f0cc3e7ffe7a0ec21cc6afc853
jigsawye <evan.y@inwinstack.com>
2016-03-17 16:38:18 +0800

    Integrate the new UI design
    * Update SignInController for new UI.
    * Update ng attributes of signin.html.

--- src/components/auth/signin/signin.controller.js
+++ src/components/auth/signin/signin.controller.js
@@ -2,16 +2,24 @@ export default class SignInController {
   /** @ngInject */
   constructor($auth, $state, $toast) {
     Object.assign(this, {
-      $auth, $state, $toast,
+      $auth, $state, $toast, credentials: {},
     });
   }
 
+  /**
+   * Log in a exists user.
+   *
+   * @return void
+   */
   submit() {
     this.$auth.login(this.credentials)
       .then(() => {
         this.$state.go('dashboard');
         this.$toast.show('Sign In Success!');
       })
-      .catch(() => (this.form.$submitted = false));
+      .catch(() => {
+        this.form.$submitted = false;
+        this.incorrectd = true;
+      });
   }
 }
