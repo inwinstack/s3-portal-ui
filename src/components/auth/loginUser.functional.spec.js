d65aded947d0cfd4fdb4711708455651d4bd67e6
chaoen <chaoen.l@inwinstack.com>
2016-03-29 07:17:00 +0000

    Remove something should do in unit test and change describe, it usage. Remove unit test thing in loginUser and registerUser. Change describe and it usage.

--- src/components/auth/loginUser.functional.spec.js
+++ src/components/auth/loginUser.functional.spec.js
@@ -4,7 +4,7 @@ import signInTemplate from './signin/signin.html';
 import directives from '../../directives/email';
 import app from './../../index.js';
 
-describe('login user functional test', function() {
+describe('User log in', function() {
   let $rootScope;
   let makeController;
   let makeTemplate;
@@ -39,71 +39,46 @@ describe('login user functional test', function() {
     makeController = () => {
       return new signInCtrl($auth, $state, $toast);
     };
-}));
-  it('signin form test', function() {
-    const controller = makeController();
-
-    form.email.$setViewValue('eeeiii');
-    $rootScope.$digest();
-    expect(form.email.$valid).to.eq(false);
-    expect(form.email.$viewValue).to.eq('eeeiii');
-    expect(form.$invalid).to.eq(true);
-
-    form.email.$setViewValue('');
-    $rootScope.$digest();
-    expect(form.email.$valid).to.eq(false);
-    expect(form.email.$viewValue).to.eq('');
-    expect(form.$invalid).to.eq(true);
-
-    form.email.$setViewValue('chaoeninwinstack.com');
-    $rootScope.$digest();
-    expect(form.email.$valid).to.eq(false);
-    expect(form.email.$viewValue).to.eq('chaoeninwinstack.com');
-    expect(form.$invalid).to.eq(true);
-
-    form.email.$setViewValue('chaoen.l@inwinstack.com');
-    $rootScope.$digest();
-    expect(form.email.$valid).to.eq(true);
-    expect(form.email.$viewValue).to.eq('chaoen.l@inwinstack.com');
-    expect(form.$invalid).to.eq(true);
-
-    form.password.$setViewValue('');
-    $rootScope.$digest();
-    expect(form.password.$valid).to.eq(false);
-    expect(form.password.$viewValue).to.eq('');
-    expect(form.$invalid).to.eq(true);
-
-    form.password.$setViewValue('abc123');
-    $rootScope.$digest();
-    expect(form.password.$valid).to.eq(true);
-    expect(form.password.$viewValue).to.eq('abc123');
-    expect(form.$invalid).to.eq(false);
+  }));
+  describe('when login with incorrect email and password', function() {
+    it('should let controller.incorrect be true and form.$submitted be false', function() {
+      const controller = makeController();
+      const data = { email: 'chaoen.l@inwinstack.com', password: 'abc123'};
+      controller.form = { '$submitted': true };
+      controller.credentials = data;
+      $httpBackend.expectPOST('http://163.17.136.83:8080/api/v1/auth/login', data).respond(401);
+      controller.submit();
+      $httpBackend.flush();
+      $rootScope.$digest();
+      expect(controller.form.$submitted).to.eq(false);
+      expect(controller.incorrect).to.eq(true);
+    });
   });
-  it('signin submit test', function() {
-    const controller = makeController();
-    const state = sinon.spy($state, 'go');
-    const toast = sinon.spy($toast, 'show');
-    const data = { email: 'chaoen.l@inwinstack.com', password: 'abc123'};
-
-    controller.form = { '$submitted': true };
-    controller.credentials = data;
-    $httpBackend.expectPOST('http://163.17.136.83:8080/api/v1/auth/login', data).respond(401);
-    controller.submit();
-    $httpBackend.flush();
-    $rootScope.$digest();
-    expect(controller.form.$submitted).to.eq(false);
-    expect(controller.incorrect).to.eq(true);
-
-    data.password = 'abc1234';
-    controller.credentials = data;
-    $httpBackend.expectPOST('http://163.17.136.83:8080/api/v1/auth/login', data).respond(200);
-    controller.submit();
-    $httpBackend.flush();
-    $rootScope.$digest();
-    expect(state).to.have.been.calledWith('dashboard');
-    expect(toast).to.have.been.calledWith('Sign In Success!');
+  describe('when login with correct email and password', function() {
+    it('should invoke $state.go and called with dashboard', function() {
+      const controller = makeController();
+      const data = { email: 'chaoen.l@inwinstack.com', password: 'abc1234'};
+      const state = sinon.spy($state, 'go');
+      controller.credentials = data;
+      $httpBackend.expectPOST('http://163.17.136.83:8080/api/v1/auth/login', data).respond(200);
+      controller.submit();
+      $httpBackend.flush();
+      $rootScope.$digest();
+      expect(state).to.have.been.calledWith('dashboard');
+    });
+    it('should invoke $toast.show and called with Sign In Success!', function(){
+      const controller = makeController();
+      const data = { email: 'chaoen.l@inwinstack.com', password: 'abc1234'};
+      const toast = sinon.spy($toast, 'show');
+      controller.credentials = data;
+      $httpBackend.expectPOST('http://163.17.136.83:8080/api/v1/auth/login', data).respond(200);
+      controller.submit();
+      $httpBackend.flush();
+      $rootScope.$digest();
+      expect(toast).to.have.been.calledWith('Sign In Success!');  
+    });
   });
-})
+});
 
 
 
--- src/components/auth/registerUser.functional.spec.js
+++ src/components/auth/registerUser.functional.spec.js
@@ -7,8 +7,6 @@ import app from './../../index.js';
 describe('User sign up', function() {
   let $rootScope;
   let makeController;
-  let makeDeferred;
-  let makeDirective;
   let makeTemplate
   let $toast;
   let $state;
@@ -21,7 +19,7 @@ describe('User sign up', function() {
 
   beforeEach(angular.mock.module('app'));
 
-  beforeEach(inject(($q, _$rootScope_, _$toast_, _$state_, _$auth_, _$compile_, _AuthService_, _$httpBackend_) => {
+  beforeEach(inject((_$rootScope_, _$toast_, _$state_, _$auth_, _$compile_, _AuthService_, _$httpBackend_) => {
     $rootScope = _$rootScope_;
 
     $httpBackend = _$httpBackend_;
@@ -44,94 +42,10 @@ describe('User sign up', function() {
 
     form = $rootScope.signup.form;
 
-    makeDeferred = () => {
-      return $q.defer();
-    };
-
     makeController = () => {
       return new signUpCtrl($auth, $state, $toast, AuthService);
     };
   }));
-  describe('when fill a email in invalid format', function() {
-    it('should be invalid', function() {  
-      form.email.$setViewValue('eeeiii');
-      $rootScope.$digest();
-      expect(form.email.$valid).to.eq(false);
-      expect(form.email.$viewValue).to.eq('eeeiii');
-
-      form.email.$setViewValue('');
-      $rootScope.$digest();
-      expect(form.email.$valid).to.eq(false);
-      expect(form.email.$viewValue).to.eq('');
-  
-      form.email.$setViewValue('chaoeninwinstack.com');
-      $rootScope.$digest();
-      expect(form.email.$valid).to.eq(false);
-      expect(form.email.$viewValue).to.eq('chaoeninwinstack.com');
-    });
-  });
-  describe('when fill a non-exist valid email', function() {
-    it('should be valid', function() {
-      form.email.$setViewValue('chaoen@inwinstack.com');
-      $rootScope.$digest();
-      expect(form.email.$valid).to.eq(true);
-      expect(form.email.$viewValue).to.eq('chaoen@inwinstack.com');
-    });
-    it('should invoke ckeckEmail() in signup.controller and emailIsValid should be true', function() {
-      const controller = makeController();
-      form.email.$setViewValue('chaoen@inwinstack.com');
-      const data = { email: form.email.$setViewValue };
-      controller.form = { email: { '$valid': form.email.$valid }};
-      controller.credentials = data;
-      $httpBackend.expectPOST('http://163.17.136.83:8080/api/v1/auth/checkEmail', data).respond(200);
-      controller.checkEmail();
-      $httpBackend.flush();
-      $rootScope.$digest();
-      expect(controller.emailIsValid).to.eq(true);
-    });
-  });
-  describe('when fill a exist email', function() {
-    it('should be valid', function() {
-      form.email.$setViewValue('chaoen.l@inwinstack.com');
-      $rootScope.$digest();
-      expect(form.email.$valid).to.eq(true);
-      expect(form.email.$viewValue).to.eq('chaoen.l@inwinstack.com');
-      expect(form.$invalid).to.eq(true);
-    });
-    it('should invoke ckeckEmail() in signup.controller and emailIsValid should be false', function() {
-      const controller = makeController();
-      form.email.$setViewValue('chaoen.l@inwinstack.com');
-      const data = { email: form.email.$setViewValue };
-      controller.form = { email: { '$valid': form.email.$valid }};
-      controller.credentials = data;
-      $httpBackend.expectPOST('http://163.17.136.83:8080/api/v1/auth/checkEmail', data).respond(403);
-      controller.checkEmail();
-      $httpBackend.flush();
-      $rootScope.$digest();
-      expect(controller.emailIsValid).to.eq(false);
-    });
-  });
-  describe('when fill different text in password and password_confirmation', function() {
-    it('should be invalid', function() {
-      form.password.$setViewValue('abc1234');
-      form.password_confirmation.$setViewValue('1234567');
-      $rootScope.$digest();
-      expect(form.password_confirmation.$valid).to.eq(false);
-      expect(form.password_confirmation.$viewValue).to.eq('1234567');
-      expect(form.password.$viewValue).to.eq('abc1234');
-    });
-  });
-  describe('when fill same text in password and password_confirmation', function() {
-    it('should be valid', function() {
-      form.password.$setViewValue('abc1234');
-      form.password_confirmation.$setViewValue('abc1234');
-      $rootScope.$digest();
-      expect(form.password_confirmation.$valid).to.eq(true);
-      expect(form.password_confirmation.$viewValue).to.eq('abc1234');
-      expect(form.password.$viewValue).to.eq('abc1234');
-      expect(form.password.$valid).to.eq(true);
-    });
-  });
   describe('when sign up success', function() {
     it('should invoke $state.go and called by auth.signin', function() {
       const controller = makeController();
