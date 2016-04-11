ba7bbaa2e571fdf2e3d0ccba5eada369549533b2
jigsawye <evan.y@inwinstack.com>
2016-04-11 17:07:57 +0800

    Update parameters name of $routeChangeError event

--- src/config/AuthenticateGuard.js
+++ src/config/AuthenticateGuard.js
@@ -16,8 +16,8 @@ export default ($rootScope, $state, $auth, $toast, $timeout) => {
     }
   });
 
-  $rootScope.$on('$routeChangeError', (arg1, arg2, arg3, arg4) => {
-    if (arg4.status === 404) {
+  $rootScope.$on('$routeChangeError', ($event, current, previous, rejection) => {
+    if (rejection.status === 404) {
       $timeout(() => $state.go('404'), 0);
     }
   });
