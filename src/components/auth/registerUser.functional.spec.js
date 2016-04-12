a8d6d619fca886e46ea24f9e693dbb85e41d1243
chaoen <chaoen.l@inwinstack.com>
2016-04-12 07:01:16 +0000

    Change first argument of it(). Let sign-up controller has a compile template. Remove some testing which is for unit test. Change first argument of it() with a meaningful sentence. Let sign-up controller has a compile template.

--- src/components/auth/registerUser.functional.spec.js
+++ src/components/auth/registerUser.functional.spec.js
@@ -4,16 +4,15 @@ import signUpTemplate from './signup/signup.html';
 import directives from '../../directives/email';
 import app from './../../index.js';
 
-describe('register user functional test', function() {
+describe('User sign up', function() {
   let $rootScope;
   let makeController;
-  let makeDirective;
-  let makeTemplate
   let $toast;
   let $state;
   let $auth;
   let $compile;
   let form;
+  let $http;
   let AuthService;
   let $httpBackend;
 
@@ -34,9 +33,7 @@ describe('register user functional test', function() {
 
     AuthService = _AuthService_;
 
-    makeTemplate = angular.element(signUpTemplate);
-
-    $compile(makeTemplate)($rootScope);
+    $compile(signUpTemplate)($rootScope);
 
     form = $rootScope.signup.form;
 
@@ -44,96 +41,72 @@ describe('register user functional test', function() {
       return new signUpCtrl($auth, $state, $toast, AuthService);
     };
   }));
-  it('signup form test', function() {
-    const controller = makeController();    
-
-    form.email.$setViewValue('eeeiii');
-    $rootScope.$digest();
-    expect(form.email.$valid).to.eq(false);
-    expect(form.email.$viewValue).to.eq('eeeiii');
-    expect(form.$invalid).to.eq(true);
-
-    form.email.$setViewValue('');
-    $rootScope.$digest();
-    expect(form.email.$valid).to.eq(false);
-    expect(form.email.$viewValue).to.eq('');
-    expect(form.$invalid).to.eq(true);
-
-    form.email.$setViewValue('chaoeninwinstack.com');
-    $rootScope.$digest();
-    expect(form.email.$valid).to.eq(false);
-    expect(form.email.$viewValue).to.eq('chaoeninwinstack.com');
-    expect(form.$invalid).to.eq(true);
-
-    form.email.$setViewValue('chaoen@inwinstack.com');
-    $rootScope.$digest();
-    expect(form.email.$valid).to.eq(true);
-    expect(form.email.$viewValue).to.eq('chaoen@inwinstack.com');
-    expect(form.$invalid).to.eq(true);
-
-    const data = { email : form.email.$setViewValue };
-    controller.form = { email: { '$valid': form.email.$valid }};
-    controller.credentials = data;
-    $httpBackend.expectPOST('http://163.17.136.83:8080/api/v1/auth/checkEmail', data).respond(403);
-    controller.checkEmail();
-    $httpBackend.flush();
-    $rootScope.$digest();
-    expect(controller.emailIsValid).to.eq(false);
-
-    form.email.$setViewValue('chaoen.l@inwinstack.com');
-    $rootScope.$digest();
-    expect(form.email.$valid).to.eq(true);
-    expect(form.email.$viewValue).to.eq('chaoen.l@inwinstack.com');
-    expect(form.$invalid).to.eq(true);
-    
-    data.email = form.email.$setViewValue;
-    controller.credentials = data;
-    $httpBackend.expectPOST('http://163.17.136.83:8080/api/v1/auth/checkEmail', data).respond(200);
-    controller.checkEmail();
-    $httpBackend.flush();
-    $rootScope.$digest();
-    expect(controller.emailIsValid).to.eq(true);
-
-    form.password.$setViewValue('abc1234');
-    form.password_confirmation.$setViewValue('1234567');
-    $rootScope.$digest();
-    expect(form.password_confirmation.$valid).to.eq(false);
-    expect(form.password_confirmation.$viewValue).to.eq('1234567');
-    expect(form.password.$viewValue).to.eq('abc1234');
-    expect(form.$invalid).to.eq(true);
-    
-    form.password_confirmation.$setViewValue('abc1234');
-    $rootScope.$digest();
-    expect(form.password_confirmation.$valid).to.eq(true);
-    expect(form.password_confirmation.$viewValue).to.eq('abc1234');
-    expect(form.password.$viewValue).to.eq('abc1234');
-    expect(form.password.$valid).to.eq(true);
-    expect(form.email.$valid).to.eq(true);
-    expect(form.$invalid).to.eq(false);
-    expect(controller.emailIsInvalid).to.eq(false);
+  describe('when fill a exist email', function() {
+    it('should let emailIsValid be false and emailIsInValid be true', function() {
+      const controller = makeController();
+      controller.form = form;
+      controller.form.email.$setViewValue = 'chaoen@inwinstack.com';
+      const data = { email: controller.form.email.$viewValue };
+      controller.credentials = data;
+      $httpBackend.expectPOST('http://163.17.136.83:8080/api/v1/auth/checkEmail', data).respond(403);
+      controller.checkEmail();
+      $httpBackend.flush();
+      $rootScope.$digest();
+      expect(controller.emailIsValid).to.eq(false);
+      expect(controller.emailIsInvalid).to.eq(true);
+    })
   })
-  it('controller submit test', function() {
-    const controller = makeController();
-    const state = sinon.spy($state, 'go');
-    const toast = sinon.spy($toast, 'show');
-    const data = { email: 'chaoen@inwinstack.com', password: 'abc1234' };
-    controller.credentials = data;
-    controller.form = { '$submitted': true };
-    $httpBackend.expectPOST('http://163.17.136.83:8080/api/v1/auth/register', data).respond(422);
-    controller.submit();
-    $httpBackend.flush();
-    $rootScope.$digest();
-    expect(state.called).to.eq(false);
-    expect(toast.called).to.eq(false);
-    expect(controller.form.$submitted).to.eq(false);
-
-    data.email ='chaoen.l@inwinstack.com';
-    controller.credentials = data;
-    $httpBackend.expectPOST('http://163.17.136.83:8080/api/v1/auth/register', data).respond(200);
-    controller.submit();
-    $httpBackend.flush();
-    $rootScope.$digest();
-    expect(state).to.have.been.calledWith('auth.signin');
-    expect(toast).to.have.been.calledWith('Sign Up Success!'); 
+  describe('when fill a non-exist email', function() {
+    it('should let emailIsValid be ture and emailIsInValid be false', function() {
+      const controller = makeController();
+      controller.form = form;
+      controller.form.email.$setViewValue= 'chaoen.l@inwinstack.com';
+      const data = { email: controller.form.email.$viewValue };
+      controller.credentials = data;
+      $httpBackend.expectPOST('http://163.17.136.83:8080/api/v1/auth/checkEmail', data).respond(200);
+      controller.checkEmail();
+      $httpBackend.flush();
+      $rootScope.$digest();
+      expect(controller.emailIsValid).to.eq(true);
+      expect(controller.emailIsInvalid).to.eq(false);
+    })
   })
+  describe('when sign up success', function() {
+    it('should invoke $state.go and called by auth.signin', function() {
+      const controller = makeController();
+      const state = sinon.spy($state, 'go');
+      const data = { email: 'chaoen@inwinstack.com', password: 'abc1234' };
+      controller.credentials = data;
+      $httpBackend.expectPOST('http://163.17.136.83:8080/api/v1/auth/register', data).respond(200);
+      controller.submit();
+      $httpBackend.flush();
+      $rootScope.$digest();
+      expect(state).to.have.been.calledWith('auth.signin');
+    });
+    it('should invoke $toast.show and called by Sign Up Success!', function() {
+      const controller = makeController();
+      const toast = sinon.spy($toast, 'show');
+      const data = { email: 'chaoen@inwinstack.com', password: 'abc1234' };
+      controller.credentials = data;
+      $httpBackend.expectPOST('http://163.17.136.83:8080/api/v1/auth/register', data).respond(200);
+      controller.submit();
+      $httpBackend.flush();
+      $rootScope.$digest();
+      expect(toast).to.have.been.calledWith('Sign Up Success!'); 
+    });
+  });
+  describe('when sign up fail', function() {
+    it('should let form.$submitted false', function() {
+      const controller = makeController();
+      const data = { email: 'chaoen.l@inwinstack.com', password: 'abc1234' };
+      controller.credentials = data;
+      controller.form = form;
+      controller.form.$submitted = true;
+      $httpBackend.expectPOST('http://163.17.136.83:8080/api/v1/auth/register', data).respond(403);
+      controller.submit();
+      $httpBackend.flush();
+      $rootScope.$digest();
+      expect(controller.form.$submitted).to.eq(false);
+    });
+  });
 })
