9a22f56638d8e4b39edda13e8f775e9251095781
jigsawye <evan.y@inwinstack.com>
2016-04-12 15:15:06 +0800

    Added TokenInterceptor to $httpProvider
    The interceptor will redirect the user to sign in page when the token
    has expired.

--- src/config/index.js
+++ src/config/index.js
@@ -4,12 +4,14 @@ import translate from './translate.config';
 import satellizer from './satellizer.config';
 import material from './material.config';
 import authenticateGuard from './AuthenticateGuard';
+import http from './http.config';
 
 const Config = module('app.config', [])
   .config(router)
   .config(translate)
   .config(satellizer)
   .config(material)
+  .config(http)
   .constant('Config', {
     API_URL: 'http://163.17.136.83:8080/api',
   })
