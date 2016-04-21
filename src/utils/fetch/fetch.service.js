a51f4bff2a7b008fc23b9a8e1a722a67a3bd0fea
jigsawye <evan.y@inwinstack.com>
2016-04-21 10:06:31 +0800

    Update $fetch service
    Move the $http call to a new method "request" and add some comments.
    
    * Add `request()` method in FetchService
    * Refactor `get()`, `post()`, `put()` & `delete()`

--- src/utils/fetch/fetch.service.js
+++ src/utils/fetch/fetch.service.js
@@ -5,19 +5,63 @@ export default class FetchService {
     this.API_URL = Config.API_URL;
   }
 
+  /**
+   * Return a http call by given method, entry and payload.
+   *
+   * @param  {String} method
+   * @param  {String} entry
+   * @param  {Object} payload = null
+   *
+   * @return {Promise}
+   */
+  request(method, entry, payload = null) {
+    return this.$http[method](`${this.API_URL}${entry}`, payload);
+  }
+
+  /**
+   * Send a GET request.
+   *
+   * @param  {String} entry
+   *
+   * @return {Promise}
+   */
   get(entry) {
-    return this.$http.get(`${this.API_URL}${entry}`);
+    return this.request('get', entry);
   }
 
+  /**
+   * Send a POST request.
+   *
+   * @param  {String} entry
+   * @param  {Object}} payload
+   *
+   * @return {Promise}
+   */
   post(entry, payload) {
-    return this.$http.post(`${this.API_URL}${entry}`, payload);
+    return this.request('post', entry, payload);
   }
 
+  /**
+   * Send a PUT request.
+   *
+   * @param  {String} entry
+   * @param  {Object} payload
+   *
+   * @return {Promise}
+   */
   put(entry, payload) {
-    return this.$http.put(`${this.API_URL}${entry}`, payload);
+    return this.request('put', entry, payload);
   }
 
+  /**
+   * Send a DELETE request.
+   *
+   * @param  {String} entry
+   * @param  {Object} payload
+   *
+   * @return {Promise}
+   */
   delete(entry, payload) {
-    return this.$http.delete(`${this.API_URL}${entry}`, payload);
+    return this.request('delete', entry, payload);
   }
 }
