cf833f43864a839a870234adac267099f27c2792
jigsawye <evan.y@inwinstack.com>
2016-05-18 16:24:05 +0800

    Remove transfer import in components/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 import { module } from 'angular';
 import Layout from './layout/layout';
 import NotFound from './not-found/not-found';
-import Transfer from './transfer/transfer';
 import Auth from './auth/auth';
 import Bucket from './bucket/bucket';
 import File from './file/file';
@@ -9,7 +8,6 @@ import File from './file/file';
 const Components = module('app.components', [
   Layout,
   NotFound,
-  Transfer,
   Auth,
   Bucket,
   File,
