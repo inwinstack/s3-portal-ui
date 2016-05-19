b9832c12328ad2e7641c92047d9c561bade919cc
jigsawye <evan.y@inwinstack.com>
2016-05-19 15:45:06 +0800

    Fix request method of check email
    The check email already modify to get method, this commit just fix this
    issue.
    
    * Change post to get of checkEmail method

--- src/components/auth/auth.service.js
+++ src/components/auth/auth.service.js
@@ -11,7 +11,7 @@ export default class AuthService {
    * @return {promise}
    */
   checkEmail(email) {
-    return this.$fetch.post('/v1/auth/checkEmail', { email });
+    return this.$fetch.get(`/v1/auth/checkEmail/${email}`);
   }
 
   /**
