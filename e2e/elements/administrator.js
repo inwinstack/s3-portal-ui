aa361e193a41f3513b4185482a7771cfa6c8f461
chun wang <q60563@gmail.com>
2016-07-28 15:42:15 +0800

    Add create account e2e test

--- e2e/elements/administrator.js
+++ e2e/elements/administrator.js
@@ -6,6 +6,18 @@ function elements () {
   this.deleteUserBtn = element(by.css('[ng-click="managerNav.delete()"]'));
   this.resetUserPassword = element(by.css('[ng-click="managerNav.reset()"]'));
   this.serachUser = element(by.name('searchText'));
+  this.createUserForm = element(by.name('create.form'));
+  this.createUserTitle = element(by.css('h2[class="ng-scope"]'));
+  this.createUserEmailInput = element(by.name('email'));
+  this.createUserEmailError = element(by.css('[ng-messages="create.form.email.$error"]'));
+  this.createUserPasswordInput = element(by.name('password'));
+  this.createUserPasswordError = element(by.css('[ng-messages="create.form.password.$error"]'));
+  this.createUserPasswordConfInput = element(by.name('password_confirmation'));
+  this.createUserPasswordConfError = element(by.css('[ng-messages="create.form.password_confirmation.$error"]'));
+  this.createUserEmailExist = element(by.css('[ng-show="create.emailIsInvalid && create.showEmailCheckedMessage"]'));
+  this.checkCreateUserBtn = element(by.css('[ng-click="create.submit()"]'));
+  this.cancelCreateUserBtn = element(by.css('[ng-click="create.cancel()"]'));
+  // this.cancelCreateUserBtn = element.all(by.css('[ng-click="create.cancel()"]'));
 }
 
 module.exports = elements;
