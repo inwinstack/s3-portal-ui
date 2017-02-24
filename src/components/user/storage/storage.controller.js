0c65cb95847e780857e4b35189bf00bbc7e32541
chaoen <jason831128@gmail.com>
2017-02-24 14:47:52 +0800

    Add Feature User Quota In My Account Page

--- src/components/user/storage/storage.controller.js
+++ src/components/user/storage/storage.controller.js
@@ -1,9 +1,11 @@
 export default class StorageController {
   /** @ngInject */
-  constructor($scope, $state) {
+  constructor($scope, $state, $fetch, $translate) {
     Object.assign(this, {
-      $scope, $state,
+      $scope, $state, $fetch, $translate
     });
+
+    this.requesting = true;
 
     this.options = {
       chart: {
@@ -25,35 +27,7 @@ export default class StorageController {
       formatGB: false,
     };
 
-    this.data = [
-      {
-        key: 'Remaining',
-        y: 5000,
-      },
-      {
-        key: 'Used',
-        y: 2000,
-      },
-    ];
-
-    this.mockData = [
-      {
-        text: 'Total Storage',
-        value: this.data[0].y + this.data[1].y,
-      },
-      {
-        text: 'Remaining Storage',
-        value: this.data[0].y,
-      },
-    ];
-
-    this.mockData = this.mockData.map((x) => {
-      const object = {};
-      object.text = x.text;
-      object.value = x.value;
-      object.formatValue = Math.round(object.value / 1024 * 100) / 100;
-      return object;
-    });
+    this.getInfomation();
   }
 
   /**
@@ -63,5 +37,44 @@ export default class StorageController {
    */
   confirm() {
     this.$state.go('bucket');
+  }
+
+  getInfomation() {
+    this.$fetch.get('/v1/user/state')
+      .then(({data}) => {
+        this.drawCanvas(data);
+        this.requesting = false;
+      });
+  }
+
+  drawCanvas(data) {
+    const source = [
+      'ACCOUNT.QUOTA_REMAIN',
+      'ACCOUNT.QUOTA_USED',
+      'ACCOUNT.QUOTA_TOTAL'
+    ];
+    this.$translate(source)
+      .then(translate => {
+        this.data = [
+          {
+            key: translate[source[0]],
+            y: data.max_size_kb - data.total_size_kb
+          },
+          {
+            key:translate[source[1]],
+            y:data.total_size_kb
+          }
+        ];
+        this.canvas = [
+          {
+            text:translate[source[2]],
+            value: data.max_size_kb / 1024
+          },
+          {
+            text:translate[source[0]],
+            value: (data.max_size_kb - data.total_size_kb) / 1024
+          }
+        ];
+      })
   }
 }
